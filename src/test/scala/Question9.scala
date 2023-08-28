cb3f1b0c73880d80950e3b12c7dd8fa4311bb9d3
Kamal Singh Danu <kamalsinghdanu97@gmail.com>
2023-08-28 12:54:20 +0530

    10. Write a program to print all even numbers in range 700 to 900

---
+++ src/test/scala/Question9.scala
@@ -0,0 +1,11 @@
+object Question9 {
+  def main(args: Array[String]): Unit = {
+    var a = 0
+    val num = 700 to 900
+    for (a <-num
+         if a%2==0) {
+      println(a)
+
+      }
+    }
+  }
