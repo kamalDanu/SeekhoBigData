5469680e841c5f3b6a165b46bc5277f47bbda3c8
Kamal Singh Danu <kamalsinghdanu97@gmail.com>
2023-08-28 13:33:53 +0530

    Write a program to print all numbers which are divisible by 11 from 250 to 550

--- src/test/scala/Question9.scala
+++ src/test/scala/Question9.scala
@@ -1,11 +1,16 @@
 object Question9 {
   def main(args: Array[String]): Unit = {
-    var a = 0
-    val num = 700 to 900
-    for (a <-num
-         if a%2==0) {
-      println(a)
+    val a = 56
+    val b = 153
+    var sum =0
 
-      }
+    for (num<- a to b){
+      sum +=num
     }
+    println(sum)
+
+
+
   }
+}
+
